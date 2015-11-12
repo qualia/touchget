61007c6d3f41d4e6adf9df28a621879b07ea8dd7
Drew Carey Buglione <drew@drewb.ug>
2015-11-12 18:00:32 -0500

    WIP

--- src/ug/drewb/touchget/TouchGet.java
+++ src/ug/drewb/touchget/TouchGet.java
@@ -3,13 +3,51 @@ package ug.drewb.touchget;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class TouchGet extends Activity
-{
-    /** Called when the activity is first created. */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class TouchGet extends Activity implements Runnable {
+
+    static String[] COMMAND = new String[] {"su", "-c", "getevent"};
+
+    Thread thread;
+
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            Process getevent = Runtime.getRuntime().exec(COMMAND);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getevent.getInputStream()));
+
+            android.util.Log.d("TouchGet", "init.");
+
+            while (true) {
+                final String line = reader.readLine();
+                if (line == null) break;
+
+                android.util.Log.d("TouchGet", line);
+            }
+        } catch (java.io.IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        thread.interrupt();
+    }
+
 }
